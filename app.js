5807cb836ed762dc9a556db41ad28415d2e88de2
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-06-01 18:23:03 -0300

    Add database configuration

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+require("dotenv").config()
 const express = require("express");
 const app = express();
 
 
-app.get("/api", (req,res)=>{
+
+app.get("/api", (req, res)=>{
     res.json({
         success: 200,
         message: "It's working"
     })
 })
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
+app.listen(process.env.APP_PORT, ()=>{
+    console.log("Server is running on port ", process.env.APP_PORT)
 })
