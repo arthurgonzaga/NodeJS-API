5f8ebc309502ce1e29a16505b21b3245edd9792e
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-06-02 00:29:44 -0300

    Add User model and register end-point

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 require("dotenv").config()
 const express = require("express");
 const app = express();
+const { User } = require("./src/models/")
 
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 
 app.get("/api", (req, res)=>{
@@ -10,6 +16,13 @@ app.get("/api", (req, res)=>{
         message: "It's working"
     })
 })
+
+
+app.post('/register', async (req, res) => {
+    console.log(req.body)
+    const user = await User.create(req.body);
+    res.json(user);
+  });
 
 app.listen(process.env.APP_PORT, ()=>{
     console.log("Server is running on port ", process.env.APP_PORT)
--- src/models/index.js
+++ src/models/index.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(config);
 fs.readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model;
   });
 
